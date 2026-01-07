45bb7f07b5d8a415fce7084a72d0b786e034eb15
Idle <651462+reonZ@users.noreply.github.com>
2026-01-07 15:35:31 +0100

    fix region bahavior not being set early enough in foundry workflow

--- src/main.ts
+++ src/main.ts
@@ -6,27 +6,18 @@ import { registerPF2eApplication } from "pf2e";
 MODULE.register(id, { game: "triggerEngine" });
 
 Hooks.once("init", async () => {
-    const args = {
+    CONFIG.RegionBehavior.dataModels[MODULE.path("builtins.region")] = TriggerEngineRegionBehaviorType;
+    CONFIG.RegionBehavior.typeIcons[MODULE.path("builtins.region")] = "fa-solid fa-land-mine-on";
+
+    Hooks.callAll("triggerEngine.init", {
         registerApplication: TriggerApplication.register.bind(TriggerApplication),
-    };
-    Hooks.callAll("triggerEngine.init", args);
+    });
 
     if (game.system.id === "pf2e") {
         registerPF2eApplication();
     }
-});
 
-Hooks.once("setup", async () => {
-    CONFIG.RegionBehavior.dataModels[MODULE.path("builtins.region")] = TriggerEngineRegionBehaviorType;
-    CONFIG.RegionBehavior.typeIcons[MODULE.path("builtins.region")] = "fa-solid fa-land-mine-on";
-
-    // prepareTriggers();
     TriggerApplication.prepareApplications();
-});
-
-Hooks.once("ready", () => {
-    // await prepareModuleTriggers();
-    // prepareTriggers();
 });
 
 MODULE.apiExpose({
