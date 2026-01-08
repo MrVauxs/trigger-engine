fc328a3ae45689d28d0ec92c09aae0e9af438cbf
Idle <651462+reonZ@users.noreply.github.com>
2026-01-09 00:19:06 +0100

    add target<->user convertors

--- src/engine/builtins/convertors.ts
+++ src/engine/builtins/convertors.ts
@@ -1,4 +1,5 @@
 import { EntryConvertor } from "engine";
+import { UserPF2e, primaryPlayerOwner } from "module-helpers";
 
 export default [
     {
@@ -13,6 +14,21 @@ export default [
         output: "number",
         convertToInput: (value: number): string => {
             return String(value);
+        },
+    },
+    {
+        input: "target",
+        output: "user",
+        convertToInput: (user: UserPF2e): TargetDocuments | undefined => {
+            const actor = user.character;
+            return actor ? { actor } : undefined;
+        },
+    },
+    {
+        input: "user",
+        output: "target",
+        convertToInput: (target: TargetDocuments): UserPF2e | undefined => {
+            return primaryPlayerOwner(target.actor) ?? undefined;
         },
     },
 ] as const satisfies EntryConvertor[];
