e16f1e031192a517ca00fdf3c46f4eec43a1023f
Idle <651462+reonZ@users.noreply.github.com>
2026-01-18 02:06:39 +0100

    fix destroying sprite cache on node clear

--- src/triggers-menu/node/node.ts
+++ src/triggers-menu/node/node.ts
@@ -337,7 +337,7 @@ class BlueprintNode extends PIXI.Container {
         const removed = this.removeChildren();
 
         for (let i = 0; i < removed.length; ++i) {
-            removed[i].destroy(true);
+            removed[i].destroy();
         }
     }
 
