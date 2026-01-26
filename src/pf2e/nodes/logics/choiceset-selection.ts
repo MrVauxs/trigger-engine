ab42ca114029bfa9cc8f197f840424024f9e612a
Idle <651462+reonZ@users.noreply.github.com>
2026-01-26 23:02:55 +0100

    add choiceset-selection pf2e loci node

---
+++ src/pf2e/nodes/logics/choiceset-selection.ts
@@ -0,0 +1,54 @@
+import { IconObject } from "_zod";
+import { BaseLogicNode } from "engine";
+import { PF2eInputEntry, PF2eOutputEntry } from "pf2e";
+import { getChoiceSetSelection, ItemPF2e } from "module-helpers";
+
+class ChoicesetSelectionLogicNode extends BaseLogicNode<"out", Inputs, { selection: string }> {
+    static get type(): "choiceset-selection" {
+        return "choiceset-selection";
+    }
+
+    static get tags(): string[] {
+        return ["rule"];
+    }
+
+    static get defineInputs(): PF2eInputEntry[] {
+        return [
+            { key: "item", type: "item" },
+            { key: "flag", type: "text" },
+            { key: "option", type: "text" },
+        ];
+    }
+
+    static get defineOutputs(): PF2eOutputEntry[] {
+        return [{ key: "selection", type: "text" }];
+    }
+
+    get icon(): IconObject {
+        return { unicode: "\uf03a" };
+    }
+
+    async _execute(): Promise<boolean> {
+        const item = await this.getInputValue("item");
+
+        if (item) {
+            const flag = await this.getInputValue("flag");
+            const option = await this.getInputValue("option");
+            const selection = getChoiceSetSelection(item, { flag, option });
+
+            if (selection) {
+                this.setOutputValue("selection", selection);
+            }
+        }
+
+        return this.executeNext("out");
+    }
+}
+
+type Inputs = {
+    flag: string;
+    item?: ItemPF2e;
+    option: string;
+};
+
+export { ChoicesetSelectionLogicNode };
--- src/pf2e/nodes/logics/index.ts
+++ src/pf2e/nodes/logics/index.ts
@@ -1,6 +1,7 @@
-import { CompareOutcomesLogicNode, DistanceBetweenLogicNode } from ".";
+import { ChoicesetSelectionLogicNode, CompareOutcomesLogicNode, DistanceBetweenLogicNode } from ".";
 
+export * from "./choiceset-selection";
 export * from "./compare-outcomes";
 export * from "./distance-between";
 
-export default [CompareOutcomesLogicNode, DistanceBetweenLogicNode];
+export default [ChoicesetSelectionLogicNode, CompareOutcomesLogicNode, DistanceBetweenLogicNode];
