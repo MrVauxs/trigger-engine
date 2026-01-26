dc43a85afc5d5f1b3f6e588e201a22587bf90f22
Idle <651462+reonZ@users.noreply.github.com>
2026-01-26 23:59:55 +0100

    add is-combatant builtin condition node

--- src/engine/builtins/nodes/conditions/index.ts
+++ src/engine/builtins/nodes/conditions/index.ts
@@ -1,7 +1,8 @@
-import { IfTruthyConditionNode, ListContainsConditionNode } from ".";
+import { IfTruthyConditionNode, IsCombatantConditionNode, ListContainsConditionNode } from ".";
 
 export * from "./base";
 export * from "./if-truthy";
+export * from "./is-combatant";
 export * from "./list-contains";
 
-export default [IfTruthyConditionNode, ListContainsConditionNode] as const;
+export default [IfTruthyConditionNode, IsCombatantConditionNode, ListContainsConditionNode] as const;
---
+++ src/engine/builtins/nodes/conditions/is-combatant.ts
@@ -0,0 +1,26 @@
+import { BuiltinsInputEntry } from "engine";
+import { isCurrentCombatant } from "module-helpers";
+import { BaseConditionNode } from ".";
+
+class IsCombatantConditionNode extends BaseConditionNode<{ target?: TargetDocuments }> {
+    static get type(): "is-combatant" {
+        return "is-combatant";
+    }
+
+    static get tags(): string[] {
+        return ["combat", "combatant"];
+    }
+
+    static get defineInputs(): BuiltinsInputEntry[] {
+        return [{ key: "target", type: "target" }];
+    }
+
+    async _execute(): Promise<boolean> {
+        const target = await this.getInputValue("target");
+        const isCombatant = !!target?.actor && isCurrentCombatant(target.actor);
+
+        return this.executeNextIf(isCombatant);
+    }
+}
+
+export { IsCombatantConditionNode };
