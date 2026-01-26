aad06b30a021b8bf30e0b05351dd0c02202ba764
Idle <651462+reonZ@users.noreply.github.com>
2026-01-26 15:16:52 +0100

    add resolve-formula builtin logic node

--- src/engine/builtins/nodes/logics/index.ts
+++ src/engine/builtins/nodes/logics/index.ts
@@ -1,8 +1,9 @@
-import { BreakLoopLogicNode, CompareNumbersLogicNode, FormatTextLogicNode } from ".";
+import { BreakLoopLogicNode, CompareNumbersLogicNode, FormatTextLogicNode, ResolveFormulaLogicNode } from ".";
 
 export * from "./base";
 export * from "./break-loop";
 export * from "./compare-numbers";
 export * from "./format-text";
+export * from "./resolve-formula";
 
-export default [BreakLoopLogicNode, CompareNumbersLogicNode, FormatTextLogicNode] as const;
+export default [BreakLoopLogicNode, CompareNumbersLogicNode, FormatTextLogicNode, ResolveFormulaLogicNode] as const;
---
+++ src/engine/builtins/nodes/logics/resolve-formula.ts
@@ -0,0 +1,76 @@
+import { BaseLogicNode, BuiltinsCustomEntry, BuiltinsInputEntry, BuiltinsOutputEntry } from "engine";
+
+class ResolveFormulaLogicNode extends BaseLogicNode<"out", Inputs, Outputs, "variable", never, "formula" | "value"> {
+    static get type(): "resolve-formula" {
+        return "resolve-formula";
+    }
+
+    static get tags(): string[] {
+        return ["number"];
+    }
+
+    static get states(): string[] {
+        return ["value", "formula"];
+    }
+
+    static get defineInputs(): BuiltinsInputEntry[] {
+        return [{ key: "formula", type: "text" }];
+    }
+
+    static get defineOutputs(): BuiltinsOutputEntry[] {
+        return [
+            { key: "formula", type: "text", state: "formula" },
+            { key: "value", type: "number", state: "value" },
+        ];
+    }
+
+    static get defineCustomInputs(): BuiltinsCustomEntry[] {
+        return [
+            {
+                slug: "variable",
+                array: false,
+                types: ["number"],
+                input: {
+                    replaceLabel: true,
+                    validation: "^[a-z]+$",
+                },
+            },
+        ];
+    }
+
+    async _execute(): Promise<boolean> {
+        let formula = await this.getInputValue("formula");
+
+        if (!formula) {
+            return this.executeNext("out");
+        }
+
+        const variables = await this.getCustomInputs("variable");
+
+        for (const { label, value } of variables) {
+            const regex = new RegExp(`@${label}`, "gm");
+            formula = formula.replace(regex, value);
+        }
+
+        if (this.state === "formula") {
+            this.setOutputValue("formula", formula);
+        } else {
+            const roll = new Roll(formula);
+            const total = (await roll.evaluate()).total;
+            this.setOutputValue("value", total);
+        }
+
+        return this.executeNext("out");
+    }
+}
+
+type Inputs = {
+    formula: string;
+};
+
+type Outputs = {
+    formula: string;
+    value: number;
+};
+
+export { ResolveFormulaLogicNode };
