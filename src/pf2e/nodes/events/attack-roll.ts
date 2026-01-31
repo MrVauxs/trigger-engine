9c06b3b0d2daade0b53aa9ca0a41a5ea56d5646b
Idle <651462+reonZ@users.noreply.github.com>
2026-01-31 15:42:45 +0100

    add reroll output

--- src/pf2e/nodes/events/attack-roll.ts
+++ src/pf2e/nodes/events/attack-roll.ts
@@ -8,7 +8,7 @@ class AttackRollEvent extends BaseEventNode<never, Outputs> {
     }
 
     static get tags(): string[] {
-        return ["chat", "attack"];
+        return ["chat", "attack", "check"];
     }
 
     static get defineOutputs(): PF2eOutputEntry[] {
@@ -17,8 +17,8 @@ class AttackRollEvent extends BaseEventNode<never, Outputs> {
             { key: "target", type: "target" },
             { key: "item", type: "item" },
             { key: "outcome", type: "outcome" },
-            { key: "item", type: "item" },
             { key: "action", type: "text" },
+            { key: "reroll", type: "boolean" },
             { key: "options", type: "text", isArray: true },
         ];
     }
@@ -27,12 +27,13 @@ class AttackRollEvent extends BaseEventNode<never, Outputs> {
         return { unicode: "\uf71c" };
     }
 
-    async _execute({ action, item, options, origin, outcome, target }: AttackRollOptions): Promise<boolean> {
+    async _execute({ action, item, options, origin, outcome, reroll, target }: AttackRollOptions): Promise<boolean> {
         this.setOutputValue("action", action);
         this.setOutputValue("item", item);
         this.setOutputValue("options", options);
         this.setOutputValue("origin", origin);
         this.setOutputValue("outcome", outcome);
+        this.setOutputValue("reroll", reroll);
         this.setOutputValue("target", target);
 
         return this.executeNext("out");
