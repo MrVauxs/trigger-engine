d84f9304effa8032379807a7019486bac4258565
Idle <651462+reonZ@users.noreply.github.com>
2026-05-03 15:09:40 +0200

    add new state to damage-taken

--- src/pf2e/nodes/events/damage-taken.ts
+++ src/pf2e/nodes/events/damage-taken.ts
@@ -3,13 +3,17 @@ import { BaseEventNode } from "engine";
 import { R } from "foundry-helpers";
 import { CreateMessageHook, DamageTakenOptions, DamageTakenType, PF2eInputEntry, PF2eOutputEntry } from "pf2e";
 
-class DamageTakenEvent extends BaseEventNode<Inputs, Outputs> {
+class DamageTakenEvent extends BaseEventNode<Inputs, Outputs, never, "select" | "everything"> {
     static get type(): "damage-taken-event" {
         return "damage-taken-event";
     }
 
     static get tags(): string[] {
         return ["chat", "damage"];
+    }
+
+    static get states(): string[] {
+        return ["select", "everything"];
     }
 
     static get defineInputs(): PF2eInputEntry[] {
@@ -29,6 +33,7 @@ class DamageTakenEvent extends BaseEventNode<Inputs, Outputs> {
                     default: "damage",
                     options: CreateMessageHook.damageTakenTypes,
                 },
+                state: "select",
             },
         ];
     }
@@ -39,7 +44,18 @@ class DamageTakenEvent extends BaseEventNode<Inputs, Outputs> {
             { key: "target", type: "target" },
             { key: "item", type: "item" },
             { key: "options", type: "text", isArray: true },
+            {
+                key: "types",
+                type: "text",
+                state: "everything",
+                isArray: true,
+                tooltip: CreateMessageHook.damageTakenTypes.join(", "),
+            },
         ];
+    }
+
+    get title(): string {
+        return this.localize(this.state === "everything" ? "titles.everything" : "title") as string;
     }
 
     get icon(): IconObject {
@@ -50,8 +66,12 @@ class DamageTakenEvent extends BaseEventNode<Inputs, Outputs> {
         const requires = await this.getInputValue("requires");
         if (requires && !origin) return true;
 
-        const when = await this.getInputValue("for");
-        if (!R.isIncludedIn(when, types)) return true;
+        if (this.state === "everything") {
+            this.setOutputValue("types", types);
+        } else {
+            const when = await this.getInputValue("for");
+            if (!R.isIncludedIn(when, types)) return true;
+        }
 
         this.setOutputValue("item", item);
         this.setOutputValue("options", options);
@@ -67,6 +87,6 @@ type Inputs = {
     requires: boolean;
 };
 
-type Outputs = Omit<DamageTakenOptions, "types">;
+type Outputs = DamageTakenOptions;
 
 export { DamageTakenEvent };
