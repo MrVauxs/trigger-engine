25159dd6865451007a59ed18a950bcd9719411b4
mrvauxs <mrvauxs@gmail.com>
2026-06-07 21:25:44 +0200

    Add hook overloads

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,13 +6,21 @@ import {
     TriggerNode as _TriggerNode,
 } from "engine/index";
 
-
 declare global {
     namespace triggerEngine {
         const NodeEntry: typeof _NodeEntry;
         const NodeField: typeof _NodeField;
         const TriggerHook: typeof _TriggerHook;
         const TriggerNode: typeof _TriggerNode;
+    }
+
+    namespace Hooks {
+        function on(hook: "triggerEngine.registerApplication", callback: (register: typeof TriggerEngine.TriggerApplication.register) => void): number;
+        function on(hook: "triggerEngine.registerNodes", callback: (registerNodes: typeof TriggerEngine.TriggerApplication.registerNodes) => void): number;
+        function on(hook: "triggerEngine.registerTriggers", callback: (registerTriggers: typeof TriggerEngine.TriggerApplication.registerTriggers) => void): number;
+        function once(hook: "triggerEngine.registerApplication", callback: (register: typeof TriggerEngine.TriggerApplication.register) => void): number;
+        function once(hook: "triggerEngine.registerNodes", callback: (registerNodes: typeof TriggerEngine.TriggerApplication.registerNodes) => void): number;
+        function once(hook: "triggerEngine.registerTriggers", callback: (registerTriggers: typeof TriggerEngine.TriggerApplication.registerTriggers) => void): number;
     }
 }
 
